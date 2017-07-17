978ad356bc9e346ed3153c3cc5f62fb66ce7d9bf
Stanlee Okwii <stanleeparker12@gmail.com>
2017-07-17 20:42:11 +0300

    Added a data property to the Transplace.xml

--- src/Transplace/widget/Transplace.js
+++ src/Transplace/widget/Transplace.js
@@ -1,24 +1,14 @@
 
 define([
     "dojo/_base/declare",
-    "mxui/widget/_WidgetBase", //all widgets must inherit of. This inherits from dijit._WidgetBase, which serves as a foundation for all widgets
-    "dijit/_TemplatedMixin",//I a HTML template, and creates the widget’s DOM tree according to that template
-
-    "mxui/dom",
-    "dojo/dom",
-    "dojo/dom-prop",
-    "dojo/dom-construct",
+    "mxui/widget/_WidgetBase", 
+     "dijit/_TemplatedMixin",
     "dojo/_base/lang",
     "dojo/text",
-    "dojo/html",
-
-
     "dojo/text!Transplace/widget/template/Transplace.html"
-], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoConstruct, lang, dojoText, dojoHtml, widgetTemplate) {
+], function (declare, _WidgetBase, _TemplatedMixin, lang, dojoText, widgetTemplate) {
     "use strict";
-
-
-    // Declare widget's prototype.
+   
     return declare("Transplace.widget.Transplace", [_WidgetBase, _TemplatedMixin], {
         templateString: widgetTemplate,
 
@@ -29,54 +19,36 @@ define([
         myWordss: null,
         counter: null,
 
-        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+        // Internal variables
         _handles: null,
-        _readOnly: false,
-
-        // counter
+        _contextObject: null,
+        storedValues: null,
+        StudentData: "",
         _i: 0,
-        postCreate: function () {
-            logger.debug(this.id + ".postCreate");
 
-            this._updateRendering();
-            this._setupEvents();
-        },
-        update: function (obj, callback) {
-            logger.debug(this.id + ".update");
-
-            this._resetSubscriptions();
-            this._updateRendering(callback); // We're passing the callback to updateRendering to be called after DOM-manipulation
-        },
         //my specialized function to reverse letters 
         ReverseName: function () {
             this.myNamee.innerHTML = this.myName.value.split("").reverse().join("");
         },
         ReverseWords: function () {
-            var aaa = this.myWord.value.split(" ").reverse().join(" ");
-            this.myWordss.innerHTML = aaa;
+            this.myWordss.innerHTML = this.myWord.value.split(" ").reverse().join(" ");
         },
         increment: function (evt) {
             this.counter.innerHTML = ++this._i;
         },
-
-        // Rerender the interface.
-        _updateRendering: function (callback) {
-            logger.debug(this.id + "._updateRendering");
-            this._executeCallback(callback, "_updateRendering");
+        
+        GetName: function () {
+           /* mx.data.get({
+                guid: this._contextObject.getGuid(),
+                callback: function (object) {
+                    storedValues = object;
+                    console.log("Received MxObject with GUID " + object.getGuid());
+                }
+            });
+            return storedValues;*/
+           var bb = this._contextObject.get(this.StudentData).value;
+            this.myNamee.innerHTML = bb;
         },
-
-        _resetSubscriptions: function () {
-            logger.debug(this.id + "._resetSubscriptions");
-            // Release handles on previous object, if any.
-            this.unsubscribeAll();
-        },
-
-        _executeCallback: function (cb, from) {
-            logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
-            if (cb && typeof cb === "function") {
-                cb();
-            }
-        }
     });
 });
 
